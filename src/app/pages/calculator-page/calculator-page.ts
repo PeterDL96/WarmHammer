42c7835578a8b5e68e9662bd51566fde80e4a6d6
Pierre-Luc Doyon <p-l.doyon@hotmail.com>
2025-07-09 22:11:27 -0400

    align fields

--- src/app/pages/calculator-page/calculator-page.ts
+++ src/app/pages/calculator-page/calculator-page.ts
@@ -2,23 +2,26 @@ import { Component } from '@angular/core';
 import { Button } from "../../components/button/button";
 import { MenuBar } from "../../components/menu-bar/menu-bar";
 import { NumericalInput } from '../../components/numerical-input/numerical-input';
-import { Fluid } from 'primeng/fluid';
-
 
 @Component({
   selector: 'app-calculator-page',
-  imports: [Button, NumericalInput, MenuBar, Fluid],
+  imports: [Button, NumericalInput, MenuBar],
   templateUrl: './calculator-page.html',
   styleUrl: './calculator-page.scss'
 })
 export class CalculatorPage {
-  public weaponStats: any = {
-    range: 'melee',
-    attack: 8,
-    weaponSkill: 2,
-    strength: 16,
-    armorPenetration: 4,
-    damage: 6,
-    keywords:[]
-  };
+
+  public unit: any = {
+    name: 'Skarbrand',
+    model: [1],
+    weaponStats: {
+      range: 'melee',
+      attack: 8,
+      weaponSkill: 2,
+      strength: 16,
+      armorPenetration: 4,
+      damage: 6,
+      keywords:[]
+    }
+  }
 }
